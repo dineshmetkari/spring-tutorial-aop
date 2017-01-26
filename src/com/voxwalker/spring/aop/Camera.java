6ef9afc47368551a761d48a48ce3932d66972c17
Jason <sysbender@gmail.com>
2017-01-26 07:33:05 -0500

    aop 004 - use annotation for aop

--- src/com/voxwalker/spring/aop/Camera.java
+++ src/com/voxwalker/spring/aop/Camera.java
@@ -1,6 +1,12 @@
 package com.voxwalker.spring.aop;
 
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+
+@Component
 public class Camera {
+	
 	public void snap() {
 		System.out.println("SNAP!");
 	}
--- src/com/voxwalker/spring/aop/Logger.java
+++ src/com/voxwalker/spring/aop/Logger.java
@@ -1,6 +1,14 @@
 package com.voxwalker.spring.aop;
 
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
 public class Logger {
+	
+	@Before("execution(void com.voxwalker.spring.aop.Camera.snap())")
 	public void aboutToTakePhoto() {
 		System.out.println("about to take photo ...");
 	}
